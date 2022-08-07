de3f5a6410b99ad30843fbb96dc5cbe34df5a5a6
Yoav <yoav.sher11@gmail.com>
2022-08-07 23:23:10 +0300

    adding a responsive modal

--- js/main.js
+++ js/main.js
@@ -1,6 +1,22 @@
 'use strict'
 
 
-function onToggleMenu() {
-    document.body.classList.toggle('menu-open')
+function onOpenMenu() {
+    document.body.classList.add('menu-open')
+}
+
+function onCloseBars() {
+    document.body.classList.remove('menu-open')
+    document.body.classList.remove('modal-open')
+}
+
+function onShowModal() {
+    document.body.classList.add('modal-open')
+    
+}
+
+function onSubmit(ev){
+    ev.preventDefault()
+    document.body.classList.remove('modal-open')
+    // console.log('close modal');
 }
